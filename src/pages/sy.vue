637534924097aeb71298deda4d79c279ec8ee9db
lipan16 <lipan16@lzu.edu.cn>
2021-06-14 13:43:11 +0800

    添加页面

---
+++ src/pages/sy.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <van-badge dot>
+        <div>大额变动</div>
+    </van-badge>
+    <div>上海市某某股份有限公司转账10000万元</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "sy",
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
-import Vue    from 'vue'
+import Vue from 'vue'
 import Router from 'vue-router'
-import Home   from '../pages/home'
+import Home from '../pages/home'
 
 Vue.use(Router)
 const router = new Router({
     linkActiveClass: 'active',
     mode: 'history',
-    routes: [{
-        path: '/',
-        component: Home
-    }
+    routes: [
+        {
+            path: '/',
+            component: () => import('../pages/sy.vue')
+        },
+        {
+            path: '/home',
+            component: Home
+        }
     ]
 })
 // router.beforeEach((to, from, next) => {
