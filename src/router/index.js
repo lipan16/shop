e1beb19fc01bff2b65bbb6931c78c1de8bc7db48
lipan16 <lipan16@lzu.edu.cn>
2021-06-28 20:29:10 +0800

    改名

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,75 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '../pages/home'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../pages/home';
 
-Vue.use(Router)
-const router = new Router({
+Vue.use(VueRouter);
+const router = new VueRouter({
     linkActiveClass: 'active',
     mode: 'history',
     routes: [
         {
+            name: '画像',
             path: '/',
             component: () => import('../pages/hx.vue')
         },
         {
+            name: '同业首页',
             path: '/sy',
-            component: () => import('../pages/sy.vue')
+            // 所有组件都打包在同个异步块 group_by
+            component: () => import(/* webpackChunkName: "group_by" */ '../pages/sy.vue')
         },
         {
+            name: '首页',
             path: '/home',
             component: Home
         }
-    ]
-})
+    ],
+    // only available in html5 history mode, 返回时期望滚动到哪个的位置
+    scrollBehavior(to, from, savedPosition){
+        if (savedPosition) {
+            // savedPosition is only available for popstate navigations.
+            return savedPosition
+        } else {
+            const position = {}
+
+            // scroll to anchor by returning the selector
+            if (to.hash) {
+                position.selector = to.hash
+                position.behavior = 'smooth'
+                // specify offset of the element
+                if (to.hash === '#anchor2') {
+                    position.offset = { y: 100 }
+                }
+
+                // bypass #1number check
+                if (/^#\d/.test(to.hash) || document.querySelector(to.hash)) {
+                    return position
+                }
+
+                // if the returned position is falsy or an empty object,
+                // will retain current scroll position.
+                return false
+            }
+
+            return new Promise(resolve => {
+                // check if any matched route config has meta that requires scrolling to top
+                if (to.matched.some(m => m.meta.scrollToTop)) {
+                    // coords will be used if no selector is provided,
+                    // or if the selector didn't match any element.
+                    position.x = 0
+                    position.y = 0
+                }
+
+                // wait for the out transition to complete (if necessary)
+                this.app.$root.$once('triggerScroll', () => {
+                    // if the resolved position is falsy or an empty object,
+                    // will retain current scroll position.
+                    resolve(position)
+                })
+            })
+        }
+    }
+});
 // router.beforeEach((to, from, next) => {
 //     document.title = to.meta.title || 'vue-manage-system'
 //     const role = localStorage.getItem('loginInfo')
@@ -40,4 +89,4 @@ const router = new Router({
 //     }
 // })
 
-export default router
+export default router;
